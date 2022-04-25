215eada6a2aeb92bd8fb7251d1ead3e734ceb007
Jareth <31932204+JustJarethB@users.noreply.github.com>
2022-04-25 15:12:41 +0100

    add redux. add simple redux implement. add shuffle

---
+++ src/reducers/deck.js
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const suits = ['C', 'H', 'S', 'D'];
+const values = ['AA', '02', '03', '04', '05', '06', '07', '08', '09', '10', 'JJ', 'QQ', 'KK'];
+const generateDeck = () => suits.map(s => values.map(v => ({ id: `${s}${v}` }))).reduce((acc, v) => acc.concat(v), []);
+
+export const deckSlice = createSlice({
+    name: 'deck',
+    initialState: generateDeck(),
+    reducers: {
+        // Redux Toolkit allows us to write "mutating" logic in reducers. It
+        // doesn't actually mutate the state because it uses the Immer library,
+        // which detects changes to a "draft state" and produces a brand new
+        // immutable state based off those changes
+        shuffle: (state) => {
+            state.sort(() => Math.random() - 0.5); // TODO: Optimise;
+
+        },
+        draw: (state) => {
+            console.log('draw')
+        },
+        sort: (state, action) => {
+            console.log('sort')
+            switch (action.payload) {
+                case 'CHASE':
+                    return;
+                case 'INIT': //fall through
+                default:
+                    return;
+            }
+        },
+    },
+})
+
+// Action creators are generated for each case reducer function
+export const { shuffle, draw, sort } = deckSlice.actions
+
+export default deckSlice.reducer
---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
+import deck from './deck';
+
+export default { deck: deck };
